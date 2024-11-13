5b0a6dfdaef6acf5ef5cee90a0e2da962a29a82b
Divyansh Singh <divyanshsingh@Divyanshs-MacBook-Air.local>
2024-11-13 11:47:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+document.querySelectorAll(".learn-more").forEach(button => {
+    button.addEventListener("click", () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        });
+    });
+});
+
+
+document.querySelectorAll(".learn-more").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Learn more about this feature!");
+    });
+});
+
+
+document.querySelectorAll(".learn-more").forEach(button => {
+    button.addEventListener("click", () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        });
+    });
+});
